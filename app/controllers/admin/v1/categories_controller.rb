fe264cb99997280783561a41a3182d9bec488541
Manoel Carlo Bertin <manoelcarlobertin@outlook.com>
2024-12-15 20:04:29 -0300

    edit show e destroy categories controller

--- app/controllers/admin/v1/categories_controller.rb
+++ app/controllers/admin/v1/categories_controller.rb
@@ -1,8 +1,23 @@
 module Admin::V1
-  class CategoriesController < ApplicationController
+  class CategoriesController < ApiController
+    before_action :load_category, only: [ :update, :destroy ]
+
     def index
       @categories = Category.all
     end
+
+    def new
+      @category = Category.new
+    end
+
+    def edit; end
+
+    # def search
+    #   @categories = Category.search(params[:query])
+    # end
+
+    # def autocomplete
+    # end
 
     def create
       @category = Category.new
@@ -10,7 +25,24 @@ module Admin::V1
       save_category!
     end
 
+    def show; end
+
+    def update
+      @category.attributes = category_params
+      save_category!
+    end
+
+   def destroy
+      @category.destroy!
+    rescue
+      render_error(fields: @category.errors.messages)
+    end
+
     private
+
+    def load_category
+      @category = Category.find(params[:id])
+    end
 
     def category_params
       return {} unless params.has_key?(:category)
