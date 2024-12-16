cd0535a962d39364d2dcba3f83c33c3f6fd6821e
Manoel Carlo Bertin <manoelcarlobertin@outlook.com>
2024-12-15 21:53:34 -0300

    configurar estes **shared examples** para não-autenticados nos endpoints de categorias

---
+++ spec/shared_examples/unauthenticated_access_example.rb
@@ -0,0 +1,8 @@
+# pra os exemplos não-autenticados, vamos apenas validar se está retornando o status *401* (**:unauthenticated**)
+RSpec.describe "Admin V1 Categories without authentication", type: :request do
+  shared_examples "unauthenticated access" do
+    it "returns unauthorized status" do
+      expect(response).to have_http_status(:unauthorized)
+    end
+  end
+end
