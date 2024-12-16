b3b46abb2a08b0ffe83eeb197014de795eba96b3
Manoel Carlo Bertin <manoelcarlobertin@outlook.com>
2024-12-15 21:46:02 -0300

    testes feitos para o perfil *client*, onde esperamos o acesso probido

--- spec/requests/admin/v1/categories/client_requests_spec.rb
+++ spec/requests/admin/v1/categories/client_requests_spec.rb
@@ -8,22 +8,26 @@ RSpec.describe "Admin V1 Categories as :client", type: :request do
     before(:each) { get url, headers: auth_header(user) }
     let(:url) { "/admin/v1/categories" }
     let!(:categories) { create_list(:category, 5) }
+    include_examples "forbidden access"
   end
 
   context "POST /categories" do
     before(:each) { post url, headers: auth_header(user) }
     let(:url) { "/admin/v1/categories" }
+    include_examples "forbidden access"
   end
 
   context "PATCH /categories/:id" do
     before(:each) { patch url, headers: auth_header(user) }
     let(:category) { create(:category) }
     let(:url) { "/admin/v1/categories/#{category.id}" }
+    include_examples "forbidden access"
   end
 
   context "DELETE /categories/:id" do
     before(:each) { delete url, headers: auth_header(user) }
     let!(:category) { create(:category) }
     let(:url) { "/admin/v1/categories/#{category.id}" }
+    include_examples "forbidden access"
   end
 end
