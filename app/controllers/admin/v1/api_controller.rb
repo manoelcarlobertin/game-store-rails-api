8c5214b9ae790f70a709b9a0f38d77dfc706726e
Manoel Carlo Bertin <manoelcarlobertin@outlook.com>
2024-12-16 22:18:19 -0300

    Isolou num concern a renderização de erros e importou para o ApiController.

--- app/controllers/admin/v1/api_controller.rb
+++ app/controllers/admin/v1/api_controller.rb
@@ -4,18 +4,15 @@ module Admin::V1
 
       include Authenticatable
 
+      # Aqui estamos importando o concern que rendereza os erros
+      include SimpleErrorRenderable
+      self.simple_error_partial = "shared/simple_error"
+
       rescue_from ForbiddenAccess do
         render_error(message: "Forbidden access", status: :forbidden)
       end # verifique se usuário tem esse perfil e caso não, lance a exceção **ForbidenAccess**
 
       before_action :restrict_access_for_admin! # callback verificador se perfil admin
-
-      def render_error(message: nil, fields: nil, status: :unprocessable_entity)
-        errors = {}
-        errors["fields"] = fields if fields.present?
-        errors["message"] = message if message.present?
-        render json: { errors: errors }, status: status
-      end
 
       private
       # agora temos o acesso restrito apenas ao usuário **:admin**.
