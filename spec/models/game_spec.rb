e90117bda01fa9e86c6c7a7e05899aef841b7363
Manoel Carlo Bertin <manoelcarlobertin@outlook.com>
2024-12-18 02:06:16 -0300

    limpei alguns testes a mais em game-spec

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -20,26 +20,6 @@ RSpec.describe Game, type: :model do
       expect(Game.modes['pvp']).to eq(1)
       expect(Game.modes['pve']).to eq(2)
       expect(Game.modes['both']).to eq(3)
-     end
-  end
-
-  # Testando mensagens de erro
-  describe 'mensagens de erro' do
-    let(:game) { Game.new }
-
-    it 'retorna mensagem de erro quando mode está ausente' do
-      game.valid?
-      expect(game.errors[:mode]).to include("can't be blank")
-    end
-
-    it 'retorna mensagem de erro quando release_date está ausente' do
-      game.valid?
-      expect(game.errors[:release_date]).to include("can't be blank")
-    end
-
-    it 'retorna mensagem de erro quando developer está ausente' do
-      game.valid?
-      expect(game.errors[:developer]).to include("can't be blank")
     end
   end
 end
