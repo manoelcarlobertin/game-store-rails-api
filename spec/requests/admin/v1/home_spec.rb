1916dd91bca22006116ff70ed54ddd291e87f653
Manoel Carlo Bertin <manoelcarlobertin@outlook.com>
2024-12-19 00:29:23 -0300

    Add um teste para verificar o que acontece quando a requisição é feita SEM um usuário autenticado ou com um token inválido!

--- spec/requests/admin/v1/home_spec.rb
+++ spec/requests/admin/v1/home_spec.rb
@@ -1,15 +1,28 @@
 require "rails_helper"
-
+# teste para a rota da home
 describe "Home", type: :request do
   let(:user) { create(:user) }
 
-  it "tests home" do
-    get '/admin/v1/home', headers: auth_header(user)
+  before do
+      get '/admin/v1/home', headers: auth_header(user)
+  end
+
+  # Verificando tanto o corpo da resposta (body_json)
+  it "returns a success message 'Uhul!'" do
     expect(body_json).to eq({ 'message' => 'Uhul!' })
   end
 
-  it "tests home" do
-    get '/admin/v1/home', headers: auth_header(user)
+  it "returns HTTP status ok (200)" do
     expect(response).to have_http_status(:ok)
+  end
+
+  context "when user is not authenticated" do
+    before do
+      get '/admin/v1/home' # Sem headers
+    end
+
+    it "returns HTTP status unauthorized (401)" do
+      expect(response).to have_http_status(:unauthorized)
+    end
   end
 end
